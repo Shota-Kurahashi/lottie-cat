08105937e163a44e87f66356483b372ad4d137b5
Shota-Kurahashi <Myheroes0715@gmail.com>
2022-12-20 11:29:55 +0900

    build

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,1 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import "../styles/style.scss";
+import Lottie from "lottie-web";
+
+const load = document.getElementById("load") as HTMLDivElement;
+
+const lottieObj = Lottie.loadAnimation({
+  container: document.getElementById("load") as HTMLDivElement, // 表示させたい要素を渡します
+  renderer: "svg", // 描画形式を指定します. // svg or canvas or html svg以外ちゃんとためしてないですが
+  loop: true, // trueにしたらループです。1回再生の場合はfalseで
+  autoplay: true, // 自動再生です。falseの場合は自分のタイミングで
+  path: "./data.json", // 再生させたいアニメーションのjsonのパスを指定します。
+});
+
+window.addEventListener(
+  "load",
+  () => {
+    setTimeout(() => {
+      lottieObj.pause();
+      load.classList.add("load--loaded");
+    }, 1000);
+  },
+  false
+);
